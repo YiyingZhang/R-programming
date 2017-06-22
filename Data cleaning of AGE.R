bbc49cdabbe5cbc5b5163fa99719a0bc64aa9d2d
YiyingZhang <zhangyiying321@hotmail.com>
2017-06-22 14:40:15 +0200

    Update Data cleaning of AGE.R

--- Data cleaning of AGE.R
+++ Data cleaning of AGE.R
@@ -4,3 +4,14 @@ default$AGE[default$AGE>50]<-"Elder"
 default$AGE[default$AGE>=30 & default$AGE<=50]<-"Middle Age"
 default$AGE[default$AGE<30]<-"Young"
 default$AGE
+
+##outliers
+##from B_1 to P_A4,all variables have outliers, replace these outliers with thier upper bond
+lower.quartile<-as.numeric(summary(default$B_1)[2])
+upper.quartile<-as.numeric(summary(default$B_1)[5])
+IQR<-upper.quartile-lower.quartile
+upper.bound<-upper.quartile+1.5*IQR
+message("upper.bound on this variable is ",upper.bound)
+default$B_1[default$B_1>upper.bound]<-upper.bound
+
+summary(default$B_1)# now the max value of B_1 is the upper bond
